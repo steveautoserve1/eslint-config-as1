43d1277af139979e64f448bfec1665e8491d5b66
JulianMayorga <mayorga.julian@gmail.com>
2017-04-26 17:39:50 -0300

    Create common eslint config

---
+++ index.js
@@ -0,0 +1,182 @@
+module.exports = {
+  "rules": {
+    "no-process-exit": 1,
+    "no-unused-vars": 1,
+    "one-var": [
+      "error",
+      {
+        "var": "never"
+      }
+    ],
+    "indent": [
+      "error",
+      2,
+      { "SwitchCase": 1 }
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "comma-dangle": [
+      2,
+      "never"
+    ],
+    "no-cond-assign": [
+      2,
+      "always"
+    ],
+    "no-console": 2,
+    "no-constant-condition": 2,
+    "no-control-regex": 1,
+    "no-debugger": 1,
+    "no-dupe-args": 2,
+    "no-dupe-keys": 2,
+    "no-duplicate-case": 2,
+    "no-empty-character-class": 2,
+    "no-empty": 2,
+    "no-ex-assign": 2,
+    "no-extra-boolean-cast": 2,
+    "no-extra-parens": [
+      2,
+      "all"
+    ],
+    "no-extra-semi": 2,
+    "no-func-assign": 2,
+    "no-inner-declarations": [
+      2,
+      "functions"
+    ],
+    "no-invalid-regexp": 2,
+    "no-irregular-whitespace": 2,
+    "no-negated-in-lhs": 2,
+    "no-obj-calls": 2,
+    "no-regex-spaces": 1,
+    "no-sparse-arrays": 2,
+    "no-unreachable": 2,
+    "use-isnan": 2,
+    "valid-typeof": 2,
+    "no-unexpected-multiline": 2,
+    "curly": [
+      2,
+      "all"
+    ],
+    "default-case": 1,
+    "quote-props": 2,
+    "quotes": [
+      2,
+      "double",
+      "avoid-escape"
+    ],
+    "no-else-return": 2,
+    "no-eval": 1,
+    "no-extend-native": 2,
+    "no-extra-bind": 1,
+    "no-fallthrough": 2,
+    "no-floating-decimal": 2,
+    "no-implied-eval": 2,
+    "no-invalid-this": 2,
+    "no-iterator": 2,
+    "no-labels": 2,
+    "no-lone-blocks": 2,
+    "no-loop-func": 1,
+    "no-multi-spaces": 2,
+    "no-native-reassign": 2,
+    "no-new-func": 2,
+    "no-new-wrappers": 2,
+    "no-new": 2,
+    "no-octal-escape": 2,
+    "no-octal": 2,
+    "no-param-reassign": [
+      1,
+      {
+        "props": false
+      }
+    ],
+    "no-proto": 2,
+    "no-redeclare": 2,
+    "no-return-assign": [
+      2,
+      "always"
+    ],
+    "no-script-url": 2,
+    "no-self-compare": 2,
+    "no-sequences": 2,
+    "no-throw-literal": 2,
+    "no-unused-expressions": 1,
+    "no-useless-call": 2,
+    "no-void": 2,
+    "no-with": 2,
+    "wrap-iife": [
+      2,
+      "inside"
+    ],
+    "yoda": [
+      2,
+      "never",
+      {}
+    ],
+    "no-catch-shadow": 2,
+    "no-label-var": 2,
+    "no-shadow-restricted-names": 2,
+    "no-undef-init": 2,
+    "handle-callback-err": 2,
+    "no-mixed-requires": 2,
+    "no-new-require": 2,
+    "brace-style": [
+      2,
+      "1tbs",
+      {}
+    ],
+    "camelcase": [
+      1,
+      {
+        "properties": "always"
+      }
+    ],
+    "comma-spacing": [
+      2,
+      {
+        "after": true
+      }
+    ],
+    "comma-style": 2,
+    "computed-property-spacing": [
+      2,
+      "never"
+    ],
+    "consistent-this": [
+      2,
+      "self"
+    ],
+    "new-parens": 2,
+    "newline-after-var": [
+      2,
+      "always"
+    ],
+    "no-array-constructor": 2,
+    "no-continue": 2,
+    "no-mixed-spaces-and-tabs": 2,
+    "no-multiple-empty-lines": [
+      2,
+      {
+        "max": 1
+      }
+    ],
+    "no-nested-ternary": 2,
+    "no-spaced-func": 2,
+    "no-unneeded-ternary": 2,
+    "object-curly-spacing": [
+      2,
+      "always",
+      {}
+    ],
+    "space-in-parens": [
+      2,
+      "never"
+    ]
+  }
+};
